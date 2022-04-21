2c55ea33893c2287bc81155bc481a4ef4613bbca
adarshiert <93669742+adarshiert@users.noreply.github.com>
2022-04-21 16:33:35 +0530

    Add files via upload

---
+++ Automated whatsapp.py
@@ -0,0 +1,38 @@
+import time
+
+import pywhatkit as kit
+import cv2
+
+
+# kit.sendwhatmsg(phone_no="+918004723595",message="jai mata di adarsh",time_hour=23,time_min=47,wait_time=27,tab_close=True,close_time=15)
+
+
+# kit.search("mirzapur")
+# kit.sendwhats_image(receiver="+918004723595",img_path="ironman.jpg",caption="jai mata di",wait_time=40,tab_close=True,close_time=15)
+
+# kit.text_to_handwriting("jai mata di",save_to="meshow.png",rgb=(0,0,138))
+# img=cv2.imread("meshow.png")
+# cv2.imshow("Text to Handwriting",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# info=kit.info("how to becomes a hacker",lines=4,return_value=True)
+# print(info)
+
+
+
+# kit.image_to_ascii_art("ironman.jpg","ironman_asciiart.txt")
+
+# kit.sendwhatmsg_to_group_instantly(group_id="Active members",message="jai mata di",wait_time=27,tab_close=True,close_time=10)
+# time.sleep(120)
+# print("message has been sent")
+# kit.take_screenshot(file_name="pywhatkit_screenshot",delay=3,show=True)
+sender="itsonlyforeductionalpurpose@gmail.com"
+passw="defaulter_of_iert"
+
+sub="checking"
+msg="you are best adarsh"
+
+reciever="adarshtiwari.1mzp@gmail.com"
+
+kit.send_mail(sender,passw,sub,msg,reciever)
